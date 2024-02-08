aba04cbe73c8dbf6a7c64f5b1db867b11a50cf1f
joaomaria04 <joaomaria.ferrei04@gmail.com>
2024-02-08 11:15:11 +0000

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -1,6 +1,8 @@
 function notas() {
-    nota = 9.4;
-    if (nota >= 9.5) {
+    nota_teorica = 9.4;
+    nota_pratica = 9.5
+    media_notas = nota_teorica + nota_pratica / 2;
+    if (media_notas >= 9.5) {
         console.log("O aluno está Aprovado.");
     }else {
         console.log("O aluno está Reprovado");
