ad0d6e53e2c355fa20da9e4ad2c600d257af5634
joaomaria04 <joaomaria.ferrei04@gmail.com>
2024-02-26 10:18:33 +0000

    Create Ficha03.js

---
+++ Ficha03/Ficha03.js
@@ -0,0 +1,21 @@
+function started() {
+    console.log("Started Download");
+}
+
+function update(i) {
+    console.log(i+"% of Download");
+}
+
+function completed() {
+    console.log("The Download is completed!");
+}
+
+function performDownload(started, update, completed) {
+    started();
+    for (i = 1; i <= 100; i++) {
+        update(i);
+    }
+    completed();
+}
+
+performDownload();
