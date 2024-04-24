0dd9a7d04e35f4da50b0871d2ff4cc44f0b1a81c
joaomaria04 <joaomaria.ferrei04@gmail.com>
2024-04-24 16:32:19 +0100

    ad

--- Ficha09/app.js
+++ Ficha09/app.js
@@ -51,7 +51,9 @@ sequelize.sync({ force: false })
     }).then(function (persons) {
         console.log(persons);
     });
+
 /*
+
 Person.bulkCreate([
     { Firstname: 'Joao', Lastname: 'Ferreira', Profession: 'Student', Age:20 },
     { Firstname: 'Paulo', Lastname: 'Andrade', Profession: 'IT Teacher', Age:34 },
@@ -63,6 +65,7 @@ Person.bulkCreate([
 });   
     
 */
+
 app.get("/persons", (req, res)=>{
   
   Person.findAll().then(persons => {
@@ -71,58 +74,30 @@ app.get("/persons", (req, res)=>{
 
 });
 
-app.post("/persons", (req, res)=>{
-  var values = [req.body.firstname, req.body.lastname, req.body.profession, req.body.age];
-  connection.query("insert into ficha7.persons(Firstname, Lastname, Profession , Age)", values, function(error, results, fields){
-    if (error) throw error;
-    res.send("Inserted with ID: " + results.insertId);
-  });
-});
-
-
-app.delete("/persons", (req, res)=>{
-  var id = req.body.id;
-  connection.query("DELETE FROM ficha07.persons WHERE id = ?", [id], function(error, results, fields){
-    if (error) throw error;
-    res.send(results);
-  });
-})
-
-app.delete("/persons/:id", (req, res)=>{
-  var id = req.params.id;
-  connection.query("DELETE FROM ficha07.persons WHERE id = ?", [id], function(error, results, fields){
-    if (error) throw error;
-    res.send(results);
-  });
-})
-
-app.get("/persons/:id", (req, res)=>{
-  connection.query("SELECT * FROM persons WHERE id = ? ", function(error, results, fields){
-    if (error) throw error;
-    res.send(results);
-  });
-});
-
-app.get("/persons/:Age/:Profession", (req, res)=>{
-  connection.query("SELECT * FROM persons WHERE Age = ? AND Profession = ? ", function(error, results, fields){
-    if (error) throw error;
-    res.send(results);
-  });
-});
-
-app.put("/persons/:id", (req, res)=>{
+app.post("/persons", (req, res) => {
   var details = req.body;
-  var id = req.params.id;
-  var updateStatement = "UPDATE ficha07.persons SET Firstname = ?, Lastname = ?, Profession = ?, Age = ? WHERE id = ?";
-  var values = [details.firstname, details.lastname, details.profession, details.age, id];
-  connection.query(updateStatement, values, function(error, results, fields){
-    if (error) 
-      res.status(500).send(JSON.stringify(error));
-    else
-      res.send("Updated Rows: " + results.affectedRows);
+  Person.create(details).then(newPerson => {
+    res.send(newPerson);
   });
 });
+
+app.delete("/persons", (req, res) => {
+  var id = req.body.id;
+  Person.destroy({
+     where: {
+       id: id 
+      } 
+    }).then(person => {
+      res.send(person);
+  });
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
